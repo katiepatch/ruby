1f4fe98248fdac8110caadf53c4112c67c4d9ea5
cocoswank <patchkatie@gmail.com>
2015-02-09 07:11:49 -0600

    Added comment

--- copycat.rb
+++ copycat.rb
@@ -1,4 +1,7 @@
-
+# Did your brother or sister ever copycat everything you said just to get under your skin?
+# Man, was that annoying or what?! Let’s write a program that does the same thing.
+# This annoying program can only be stopped if the user says “I’m  a dummy”
+# because the program won’t repeat something so self deprecating!
 
 while true
   input = gets.chomp
