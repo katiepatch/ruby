02b70919cc80f74b1addd3a88467531f122ab96c
cocoswank <patchkatie@gmail.com>
2015-02-09 17:36:20 -0600

    Fixed Bugs

--- santa.rb
+++ santa.rb
@@ -1,61 +1,56 @@
-# Creating a Secret Santa App
+# Secret Santa Generator
+# RULES:
+    # 1. Cannot receive self.
+    # 2. Must give everyone a pair.
+    # 3. Same person cannot be a giver twice or a receiver twice.
 
-# list people involved
+#-----------------------------------------------------------------------
+def secret_santa(input)
 
+  # Create copy of array and shuffle.
+  input_copy = input.dup
+  output = input_copy.shuffle!
+  array = []
 
-giver = {}
-total_people = 0
-
-while true
-  input = gets.chomp
-  if input != "done"
-    giver[input] = ""
-  else
-    break
+  # Match givers to receivers.
+  entry = 0
+  x = output.length
+  while entry != input.length
+    input_final = input[entry]
+    output_final = output.last
+     if input_final != output_final
+      array.push("#{input_final} has #{output_final}.")
+      output.pop
+      end
+    entry += 1
   end
-  total_people += 1
+
+  # Force recursion if missing lines.
+  if array[entry - 1] == nil
+    array.clear
+    secret_santa(input)
+  end
+
+  puts array
+
 end
 
+# Initial interaction:
+puts "Welcome to Secret Santa Generator!"
+puts "Please type in all of the names of participants and type 'done'."
 
+# Set up array.
+giver = []
 
-giver.each do | original, assigned|
+# Get names of people and put in an array.
+while name = gets.chomp
+    if name != "done"
+      giver.push(name)
+    else
+      break
+    end
+  end
 
-  puts "#{original} has #{assigned}."
-end
-
-puts giver
-puts giver.length
-
-
-
-
-
-# entry = 0
-
-
-# loop: rules:
-            # everyone needs somewone
-            # can't have self
-            # can't have receiver twice...sample and pop?
-
-# giver.each do |list, name|
-#   receiver_final = input[list]
-#   puts "#{list} has #{name}."
-
-# end
-
-# while entry < giver.length
-#   giver_final = giver[entry]
-#   receiver_final = receiver.last
-#     if giver_final != receiver_final
-#     puts "#{giver_final} has #{receiver_final}."
-#     receiver.pop
-#     else
-#       entry
-#     end
-#   entry += 1
-# end
-
-
-
+# Call method
+puts secret_santa(giver)
 
