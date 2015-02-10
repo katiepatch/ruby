b179e373bb251e5a1aec936c62f973d15436eee4
cocoswank <patchkatie@gmail.com>
2015-02-10 07:15:32 -0600

    Cleaned up notes

--- hash.rb
+++ hash.rb
@@ -1,19 +1,20 @@
 # A hash is an associative array
+# A symbol is used as an identifier in Ruby
+# -------------------------------------------------------------------
 
 # my_hash = {}
 # my_hash["name"] = "Dana"
 # my_hash["age"] = 34
 # my_hash["eye_color"] = "green"
 
-#populating hash item by item
+# populating hash item by item
+# -------------------------------------------------------------------
 
-# my_hash = {
-#   "name" => "Dana",
-#   "age" => 34
-#   "eye_color" => "green" }
+# my_hash = {"name" => "Dana", "age" => 34, "eye_color" => "green" }
 
 # hash rocket "=>" assigns values to keys
 # colon in front of key value enters info in as well
+# -------------------------------------------------------------------
 
 my_hash = {:name => "Dana", :age => 34, :eye_color => "green"}
 
